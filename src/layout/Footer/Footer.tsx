fa21196dd12ce1716c674c86c6ce9e60b206535c
macjjuni <macjjuni@gmail.com>
2026-02-16 13:48:29 +0900

    feat. 푸터 폰트 사이즈 수정

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
 
   return (
     <footer className="flex justify-end items-center h-10 px-3 select-none">
-      <a className="text-lg italic" target="_blank" rel="noreferrer" href="https://github.com/macjjuni">
+      <a className="text-md italic" target="_blank" rel="noreferrer" href="https://github.com/macjjuni">
         {new Date().getFullYear()} 꿀양갱갱갱.
       </a>
     </footer>
